b9d46837201e5d2480eae3757af0bf7b5d4b5e1e
SeokChan-Lee <melee0826@gmail.com>
2025-02-05 02:10:23 +0900

    :sparkles: feat: dashboard-header 컴포넌트 구현

--- src/app/mydashboard/layout.tsx
+++ src/app/mydashboard/layout.tsx
@@ -1,11 +1,12 @@
 import Sidebar from '@/components/Sidebar/Sidebar';
 import { ReactNode } from 'react';
+import Header from '@/components/dashboard-header/Header';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className='flex'>
       <Sidebar />
-
+      <Header title='나의 대시보드' showCrown showSetting showUsers showProfile />
       <main className='flex-1'>{children}</main>
     </div>
   );
---
+++ src/components/dashboard-header/Header.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import React from 'react';
+import Image from 'next/image';
+import Crown from '@/assets/icons/crown.svg';
+import Title from './Title';
+import SettingBtn from './SettingBtn';
+
+interface HeaderProps {
+  title?: string;
+  showCrown?: boolean;
+  showSetting?: boolean;
+  showUsers?: boolean;
+  showProfile?: boolean;
+}
+
+// API 연동 이후 추가 작업 예정, 우선 mock 데이터로 집어넣어놨습니다.
+interface UserType {
+  nickname: string;
+  profileImageUrl: string;
+}
+
+const mockUser: UserType = {
+  nickname: '배유철',
+  profileImageUrl: 'B',
+};
+
+export default function Header({ title, showCrown = false, showSetting = false, showUsers = false, showProfile = false }: HeaderProps) {
+  const handleSettingClick = () => {
+    console.log('관리 버튼 클릭');
+  };
+
+  const handleInviteClick = () => {
+    console.log('초대 버튼 클릭');
+  };
+
+  const handleProfileClick = () => {
+    console.log('프로필 버튼 클릭');
+  };
+
+  return (
+    <header className='flex h-[60px] w-full items-center justify-between border-b-2 border-gray-20 py-[15.5px] sm:h-[70px] sm:px-[40px]'>
+      {/* 왼쪽 제목 그룹 */}
+      <div className='flex items-center gap-[8px]'>
+        <Title title={title} />
+        {showCrown && <Image src={Crown} alt='방장 표시' className='hidden h-[16px] w-[20px] lg:block' />}
+      </div>
+      {/* 오른쪽 버튼 그룹 */}
+      <div className='flex items-center'>
+        <>
+          <div className='flex items-center gap-[16px] border-r border-gray-30 pr-[12px] sm:gap-[32px] sm:pr-[24px] md:gap-[40px] md:pr-[32px]'>
+            {showSetting && <SettingBtn onSettingClick={handleSettingClick} onInviteClick={handleInviteClick} />}
+            {showUsers && (
+              <div className='flex'>
+                <span className='-ml-[8px] flex h-[34px] w-[34px] items-center justify-center rounded-full border-2 border-white bg-green-30 text-white sm:h-[38px] sm:w-[38px]'>
+                  {mockUser.profileImageUrl}
+                </span>
+                <span className='-ml-[8px] flex h-[34px] w-[34px] items-center justify-center rounded-full border-2 border-white bg-green-30 text-white sm:h-[38px] sm:w-[38px]'>
+                  {mockUser.profileImageUrl}
+                </span>
+                <span className='-ml-[8px] flex h-[34px] w-[34px] items-center justify-center rounded-full border-2 border-white bg-green-30 text-white sm:h-[38px] sm:w-[38px]'>
+                  {mockUser.profileImageUrl}
+                </span>
+              </div>
+            )}
+          </div>
+          {showProfile && (
+            <button onClick={handleProfileClick} className='mr-[40px] flex items-center justify-center gap-[12px] pl-[12px] sm:pl-[24px] md:pl-[32px]'>
+              <span className='flex h-[34px] w-[34px] items-center justify-center rounded-full border-2 border-white bg-green-30 text-white sm:h-[38px] sm:w-[38px]'>{mockUser.profileImageUrl}</span>
+              <span className='hidden text-[16px] md:block'>{mockUser.nickname}</span>
+            </button>
+          )}
+        </>
+      </div>
+    </header>
+  );
+}
