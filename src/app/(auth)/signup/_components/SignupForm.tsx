0220a19ce4bf05036db36d7b6c9d0488b02b7f28
Tokyun02 <developtk02@gmail.com>
2025-02-04 11:14:15 +0900

    :sparkles: feat : 회원가입 form validation 로직 구현
    1. zodResolver 의존성 추가 설치
    2. 체크박스 및 버튼은 컴포넌트로 분리할 예정
    3. 회원가입 폼 관련 상수 정의

--- src/app/(auth)/signup/_components/SignupForm.tsx
+++ src/app/(auth)/signup/_components/SignupForm.tsx
@@ -1,15 +1,78 @@
 'use client';
 
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 import Field from '@/components/auth/Field';
-import { SIGNUP_FORM_PLACEHOLDER } from '@/constants/auth';
+import { SIGNUP_FORM_PLACEHOLDER, SINGUP_FORM_VALID_LENGTH, SIGNUP_FORM_ERROR_MESSAGE } from '@/constants/auth';
+import { useForm } from 'react-hook-form';
 //TODO: 회원가입 폼 레이아웃 및 validation 작업
+
+//TODO: API 함수 구현 후 스키마와 타입 정의 옮길 예정
+const signupSchema = z
+  .object({
+    email: z.string().min(SINGUP_FORM_VALID_LENGTH.EMAIL.MIN, SIGNUP_FORM_ERROR_MESSAGE.EMAIL.MIN).email(SIGNUP_FORM_ERROR_MESSAGE.EMAIL.NOT_FORM),
+    nickname: z.string().min(SINGUP_FORM_VALID_LENGTH.NICKNAME.MIN, SIGNUP_FORM_ERROR_MESSAGE.NICKNAME.MIN).max(SINGUP_FORM_VALID_LENGTH.NICKNAME.MAX, SIGNUP_FORM_ERROR_MESSAGE.NICKNAME.MAX),
+    password: z.string().min(SINGUP_FORM_VALID_LENGTH.PASSWORD.MIN, SIGNUP_FORM_ERROR_MESSAGE.PASSWORD.MIN),
+    passwordConfirm: z.string(),
+    terms: z.boolean(),
+  })
+  .refine((check) => check.password === check.passwordConfirm, {
+    message: SIGNUP_FORM_ERROR_MESSAGE.PASSWORD_CONFIRM.NOT_MATCH,
+    path: ['passwordConfirm'],
+  })
+  .refine((check) => check.terms === true, {
+    message: SIGNUP_FORM_ERROR_MESSAGE.TERMS.NOT_TOS,
+    path: ['terms'],
+  });
+
+type SignupFormData = z.infer<typeof signupSchema>;
+
 export default function SignupForm() {
+  const {
+    register,
+    trigger,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    resolver: zodResolver(signupSchema),
+    mode: 'onBlur',
+    defaultValues: {
+      email: '',
+      nickname: '',
+      password: '',
+      passwordConfirm: '',
+      terms: false,
+    },
+  });
+  const onSubmit = (signupFormData: SignupFormData) => {
+    // TODO : 디버깅 용으로 남겼습니다. API 함수 구현이 완료되면 로직 수정 예정입니다.
+    console.log(signupFormData);
+  };
+
   return (
-    <form>
-      <Field label='이메일' type='email' placeholder={SIGNUP_FORM_PLACEHOLDER.EMAIL} />
-      <Field label='닉네임' type='text' placeholder={SIGNUP_FORM_PLACEHOLDER.NICKNAME} />
-      <Field label='비밀번호' type='password' placeholder={SIGNUP_FORM_PLACEHOLDER.PASSWORD} />
-      <Field label='비밀번호 확인' type='password' placeholder={SIGNUP_FORM_PLACEHOLDER.PASSWORD_CONFIRM} />
+    <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-6'>
+      <Field label='이메일' type='email' placeholder={SIGNUP_FORM_PLACEHOLDER.EMAIL} register={register('email')} errorMessage={errors.email?.message} />
+      <Field label='닉네임' type='text' placeholder={SIGNUP_FORM_PLACEHOLDER.NICKNAME} register={register('nickname')} errorMessage={errors.nickname?.message} />
+      <Field
+        label='비밀번호'
+        type='password'
+        placeholder={SIGNUP_FORM_PLACEHOLDER.PASSWORD}
+        register={register('password', {
+          onBlur: () => trigger('passwordConfirm'),
+        })}
+        errorMessage={errors.password?.message}
+      />
+      <Field label='비밀번호 확인' type='password' placeholder={SIGNUP_FORM_PLACEHOLDER.PASSWORD_CONFIRM} register={register('passwordConfirm')} errorMessage={errors.passwordConfirm?.message} />
+      {/* TODO: 체크박스 및 버튼 컴포넌트 분리 예정*/}
+      <label className='flex items-center text-sm text-gray-700'>
+        <input type='checkbox' {...register('terms')} />
+        <span>이용약관에 동의합니다.</span>
+      </label>
+      {errors.terms && <span className='text-md text-red'>{errors.terms.message}</span>}
+
+      <button disabled={!isValid} className={`rounded-lg ${isValid ? 'cursor-pointer bg-violet-20' : 'cursor-not-allowed bg-gray-40'} py-3 text-2lg font-medium text-white`}>
+        가입하기
+      </button>
     </form>
   );
 }
--- src/components/auth/Field.tsx
+++ src/components/auth/Field.tsx
@@ -2,6 +2,7 @@ import OpendEyeIcon from '@/assets/icons/opend_eye.svg';
 import ClosedEyeIcon from '@/assets/icons/closed_eye.svg';
 import Image from 'next/image';
 import { useState } from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form';
 
 type Input = 'text' | 'email' | 'password';
 
@@ -9,6 +10,8 @@ interface FieldProps {
   label: string;
   type: Input;
   placeholder?: string;
+  errorMessage?: string;
+  register: UseFormRegisterReturn;
 }
 
 interface PasswrodType {
@@ -16,7 +19,7 @@ interface PasswrodType {
   icon: string;
 }
 
-export default function Field({ label, type, placeholder = '' }: FieldProps) {
+export default function Field({ label, type, placeholder = '', register, errorMessage }: FieldProps) {
   const [passwordType, setPasswordType] = useState<PasswrodType>({ type, icon: ClosedEyeIcon });
 
   const togglePassword = () => {
@@ -26,12 +29,13 @@ export default function Field({ label, type, placeholder = '' }: FieldProps) {
   };
 
   return (
-    <>
+    <div className='flex flex-col gap-2'>
       <label className='relative flex flex-col gap-2 text-gray-70'>
         <span>{label}</span>
-        <input type={type !== 'password' ? type : passwordType.type} placeholder={placeholder} className='rounded-lg border p-4 text-gray-70' />
+        <input type={type !== 'password' ? type : passwordType.type} placeholder={placeholder} className='rounded-lg border p-4 text-gray-70' {...register} />
         {label.includes('비밀번호') && <Image src={passwordType.icon} alt='비밀번호 숨김 아이콘' width={24} height={24} onClick={togglePassword} className='absolute right-4 top-12 cursor-pointer' />}
       </label>
-    </>
+      {errorMessage && <span className='text-md text-red'>{errorMessage}</span>}
+    </div>
   );
 }
