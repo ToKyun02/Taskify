572ddf62cd3fe8b8c268d519d9f1603f399cfa61
chanki.kim <chanki.kim89@gmail.com>
2025-02-04 14:50:48 +0900

    💄 design : privacy, fag 페이지 상단 헤더 스타일 조정

--- src/app/(public)/_components/SubHeader.tsx
+++ src/app/(public)/_components/SubHeader.tsx
@@ -1,7 +1,7 @@
 export default function SubHeader({ title, desc }: { title: string; desc: string }) {
   return (
-    <header className='mb-10 border-b border-white/15 pb-10'>
-      <h2 className='font-mont mb-4 text-3xl font-bold tracking-tight'>{title}</h2>
+    <header className='mb-10 py-20'>
+      <h2 className='mb-4 font-mont text-3xl font-bold tracking-tight'>{title}</h2>
       <p className='font-light opacity-40'>{desc}</p>
     </header>
   );
