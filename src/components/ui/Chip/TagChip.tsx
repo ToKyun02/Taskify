9a9434a02acf506416cedbdb9c1ff177c40e8ea9
chanki.kim <chanki.kim89@gmail.com>
2025-02-05 21:13:24 +0900

    💄 feat : TagChip 컴포넌트 작업

---
+++ src/components/ui/Chip/TagChip.tsx
@@ -0,0 +1,32 @@
+import { HTMLAttributes } from 'react';
+import { DEFAULT_COLORS } from '@/constants/colors';
+import { cn, getColorByString } from '@/utils/helper';
+
+/**
+ * 하나의 color 값으로 배경색과 폰트색상에 적용합니다.
+ *
+ * 받은 color가 #FFCC00일경우
+ *
+ * font color => #FFCCOO
+ * background color => #FFCCOO30 (30% opacity)
+ */
+
+const BACKGROUND_ALPHA = 30;
+
+interface TagChipProps extends HTMLAttributes<HTMLSpanElement> {
+  label: string;
+}
+
+export default function TagChip({ label, className, ...props }: TagChipProps) {
+  const colorCode = getColorByString(label, DEFAULT_COLORS);
+
+  return (
+    <span
+      className={cn('inline-flex h-[26px] items-center justify-center whitespace-nowrap rounded-[4px] px-[6px] text-xs leading-none md:h-7 md:text-md', className)}
+      style={{ color: colorCode, backgroundColor: `${colorCode}${BACKGROUND_ALPHA}` }}
+      {...props}
+    >
+      {label}
+    </span>
+  );
+}
