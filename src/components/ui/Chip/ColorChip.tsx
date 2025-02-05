ce6959d5462c60828d1d8175b1b3dbcf23603b8b
chanki.kim <chanki.kim89@gmail.com>
2025-02-05 21:14:10 +0900

    💄 feat : ColorChip, ColorPicker 컴포넌트 작업

---
+++ src/components/ui/Chip/ColorChip.tsx
@@ -0,0 +1,15 @@
+import { HexColor } from '@/constants/colors';
+import { cn } from '@/utils/helper';
+import { HTMLAttributes } from 'react';
+
+interface ColorChipProps extends HTMLAttributes<HTMLSpanElement> {
+  color: HexColor;
+}
+
+export default function ColorChip({ color, className, ...props }: ColorChipProps) {
+  return (
+    <span className={cn('inline-flex h-[30px] w-[30px] rounded-full leading-none', className)} style={{ backgroundColor: color }} {...props}>
+      <span className='sr-only'>{color}</span>
+    </span>
+  );
+}
