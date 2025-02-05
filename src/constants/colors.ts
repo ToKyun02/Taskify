5c9fe9b20a1498e89125c2de9641bc1fd821f7cd
chanki.kim <chanki.kim89@gmail.com>
2025-02-05 15:44:12 +0900

    ✨ feat : 앞글자 유니코드로 색상 선택 헬퍼 함수 작성

---
+++ src/constants/colors.ts
@@ -0,0 +1,1 @@
+export const DEFAULT_COLORS = ['#7AC555', '#760DDE', '#FFA500', '#76A5EA', '#E876EA'];
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -4,3 +4,9 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getColorByString(value: string, colorArray: string[]) {
+  const charCode = value.toLowerCase().charCodeAt(0);
+  const index = charCode % colorArray.length;
+  return colorArray[index];
+}
