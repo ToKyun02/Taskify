7e1f65592e63e1c222b8cf68f65f246186c497a0
Tokyun02 <developtk02@gmail.com>
2025-02-05 18:51:29 +0900

    :recycle: refactor : axios 상태코드 확인 로직 삭제

--- src/apis/users/index.ts
+++ src/apis/users/index.ts
@@ -1,6 +1,5 @@
 import axiosHelper from '@/utils/network/axiosHelper';
 import { SignupFormData } from './types';
-import isResponseSuccess from '@/utils/network/isResponseSuccess';
 import { isAxiosError } from 'axios';
 import { SignupResponse } from './types';
 import { isError } from 'es-toolkit/compat';
@@ -8,7 +7,6 @@ import { isError } from 'es-toolkit/compat';
 export const signup = async (signupFormData: SignupFormData): SignupResponse => {
   try {
     const response = await axiosHelper.post('/users', signupFormData);
-    if (!isResponseSuccess(response.status)) throw new Error(`${response.status} ${response.statusText}`);
     return response.data;
   } catch (error) {
     if (isAxiosError(error)) return error.response?.data;
--- src/utils/network/isResponseSuccess.ts
+++
@@ -1,4 +0,0 @@
-export default function isResponseSuccess(status: number) {
-  if (status >= 200 && status < 300) return true;
-  return false;
-}
