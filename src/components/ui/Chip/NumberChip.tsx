8620e3d4d067b3e27bf10eb24d2b2dc389d9ed11
chanki.kim <chanki.kim89@gmail.com>
2025-02-05 21:15:37 +0900

    💄 feat : Number Chip 컴포넌트 작업

---
+++ src/components/ui/Chip/NumberChip.tsx
@@ -0,0 +1,19 @@
+import { cn } from '@/utils/helper';
+import { HTMLAttributes } from 'react';
+
+interface NumberChipProps extends HTMLAttributes<HTMLSpanElement> {
+  label: number;
+  max?: number;
+}
+
+const DEFAULT_MAX_NUMBER = 99;
+
+export default function NumberChip({ label, max = DEFAULT_MAX_NUMBER, className, ...props }: NumberChipProps) {
+  const safeParseMaxNumber = max <= 0 || !Number.isInteger(max) ? DEFAULT_MAX_NUMBER : max;
+
+  return (
+    <span className={cn('inline-flex h-5 min-w-5 items-center justify-center whitespace-nowrap rounded bg-gray-20 px-1 text-xs leading-none text-gray-50', className)} {...props}>
+      {label > safeParseMaxNumber ? `${safeParseMaxNumber}+` : label}
+    </span>
+  );
+}
