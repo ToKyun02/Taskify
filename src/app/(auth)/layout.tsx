e69a9b961ed29f4e4b6d2c93ae1ed887b15681de
Tokyun02 <developtk02@gmail.com>
2025-02-06 17:49:25 +0900

    :recycle: refactor : auth 관련 페이지 로그인 상태 로직 변경

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,18 +1,7 @@
-'use client';
-
-import useAuthStore from '@/stores/authStore';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const router = useRouter();
-  const { accessToken } = useAuthStore();
-  useEffect(() => {
-    if (accessToken) router.push('/mydashboard');
-  }, [router, accessToken]);
   return <div className='mx-3 flex min-h-dvh flex-col items-center justify-center gap-9 bg-gray-10'>{children}</div>;
 }
