d90558125c04426f6bd882144c63e5111b9f5017
Tokyun02 <developtk02@gmail.com>
2025-02-06 16:23:04 +0900

    :sparkles: feat : 로그인 성공 후 /mydashboard 라우팅 구현

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,18 @@
+'use client';
+
+import useAuthStore from '@/stores/useAuthStore';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const router = useRouter();
+  const { accessToken } = useAuthStore();
+  useEffect(() => {
+    if (accessToken) router.push('/mydashboard');
+  }, [router, accessToken]);
   return <div className='mx-3 flex min-h-dvh flex-col items-center justify-center gap-9 bg-gray-10'>{children}</div>;
 }
