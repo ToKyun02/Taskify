b01002798701a0c59a5ef81b5339a6d605def0d1
chanki.kim <chanki.kim89@gmail.com>
2025-02-07 22:51:38 +0900

    ✨ feat : dashboards get, create 리액트쿼리 작성

---
+++ src/apis/dashboards/queries.ts
@@ -0,0 +1,39 @@
+'use client';
+
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { createDashboard, getDashboards } from '.';
+import { DashboardFormType } from './types';
+
+export const useDashboardsQuery = (page: number, size: number) => {
+  return useQuery({
+    queryKey: ['dashboards', page, size],
+    queryFn: () =>
+      getDashboards({
+        page,
+        size,
+        navigationMethod: 'pagination',
+      }),
+  });
+};
+
+export const useDashboardMutation = () => {
+  const queryClient = useQueryClient();
+
+  const create = useMutation({
+    mutationFn: (data: DashboardFormType) => {
+      return createDashboard(data);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['dashboards'] });
+    },
+  });
+
+  const update = () => {};
+  const remove = () => {};
+
+  return {
+    create: create.mutateAsync,
+    update,
+    remove,
+  };
+};
