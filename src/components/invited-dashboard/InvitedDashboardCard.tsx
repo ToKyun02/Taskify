b3e344c7e57ccf2dfc321c9696bf0737ab6fd364
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-02-07 16:53:41 +0900

    :sparkles: feat: 초대받은 대시보드 검색 기능 구현

--- src/components/invited-dashboard/InvitedDashboardCard.tsx
+++ src/components/invited-dashboard/InvitedDashboardCard.tsx
@@ -1,11 +1,17 @@
+'use client';
+
 import Image from 'next/image';
 import Button from '../ui/Button/Button';
 import searchIc from '@/assets/icons/search.svg';
 import empty_dashboard from '@/assets/images/empty_dashboard.png';
 import mockData from '@/apis/invitations/mockData';
+import { useState } from 'react';
 
 export default function InvitedDashboardCard() {
   const { invitations } = mockData;
+  const [searchKeyword, setSearchKeyword] = useState('');
+
+  const filteredInvitations = invitations.filter((item) => item.dashboard.title.toLocaleLowerCase().includes(searchKeyword.toLocaleLowerCase()));
 
   return (
     <div className='w-full rounded-lg bg-white py-6 md:w-full md:py-4 lg:w-[1022px] lg:py-8'>
@@ -24,7 +30,7 @@ export default function InvitedDashboardCard() {
               <h3 className='text-2xl font-bold text-gray-70'>초대받은 대시보드</h3>
               <div className='flex h-10 items-center gap-2 rounded-md border px-4 py-1.5'>
                 <Image src={searchIc} width={16} height={16} alt='search' />
-                <input placeholder='검색' className='w-full flex-1 outline-none' />
+                <input placeholder='검색' className='w-full flex-1 outline-none' value={searchKeyword} onChange={(e) => setSearchKeyword(e.target.value)} />
               </div>
             </div>
             <div className='px-8 lg:px-14'>
@@ -35,7 +41,7 @@ export default function InvitedDashboardCard() {
                   <span>수락 여부</span>
                 </div>
                 <div className='flex flex-col gap-5'>
-                  {invitations.map((item) => (
+                  {filteredInvitations.map((item) => (
                     <div key={item.id} className='grid grid-cols-[1fr_1fr_2fr] items-center gap-5 border-b pb-4 text-lg text-gray-70 lg:pl-10'>
                       <span>{item.dashboard.title}</span>
                       <span>{item.inviter.nickname}</span>
@@ -53,7 +59,7 @@ export default function InvitedDashboardCard() {
               </div>
               <div className='md:hidden'>
                 <div className='flex flex-col'>
-                  {invitations.map((item) => (
+                  {filteredInvitations.map((item) => (
                     <div key={item.id} className='border-b pb-4 pt-3.5'>
                       <div className='flex flex-col gap-3.5'>
                         <div className='flex gap-6'>
