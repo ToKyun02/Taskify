5049b42094cec541c2a4defb3ba5c83385fe9e5c
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-02-07 22:57:34 +0900

    :sparkles: feat: 초대받은 대시보드 리스트 무한 스크롤 구현

--- src/app/(dashboard)/mydashboard/page.tsx
+++ src/app/(dashboard)/mydashboard/page.tsx
@@ -1,9 +1,16 @@
+'use client';
+
 import InvitedDashboardCard from '@/components/invited-dashboard/InvitedDashboardCard';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function page() {
   return (
     <div>
-      <InvitedDashboardCard />
+      <QueryClientProvider client={queryClient}>
+        <InvitedDashboardCard />
+      </QueryClientProvider>
     </div>
   );
 }
