1cef3abe4b9bea1ecd8ff480f04f6474de7fa6e9
chanki.kim <chanki.kim89@gmail.com>
2025-02-07 23:10:51 +0900

    💄 style : Sidebar 모바일 css 수정 (로고 마진)

--- src/components/dashboard/SidebarLogo.tsx
+++ src/components/dashboard/SidebarLogo.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 export default function SidebarLogo() {
   return (
-    <Link href='/' className='mb-[14px] flex justify-center md:mb-14 md:justify-start'>
+    <Link href='/' className='mb-[38px] flex justify-center md:mb-14 md:justify-start'>
       <Image src={logo} width={108} height={33} alt='logo' className='hidden md:flex' />
       <Image src={logo_ci} width={23} height={25} alt='logo' className='h-[25px] w-[23px] md:hidden' />
     </Link>
