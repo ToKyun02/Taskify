b612d835328ed02488c845b903ff7a239aa60edc
chanki.kim <chanki.kim89@gmail.com>
2025-02-07 23:05:21 +0900

    ✨ feat : 내 대시보드 페이지 상단 부분 작업

--- src/app/(dashboard)/mydashboard/page.tsx
+++ src/app/(dashboard)/mydashboard/page.tsx
@@ -1,16 +1,16 @@
-'use client';
-
+import MyDashboard from '@/components/dashboard/MyDashboard';
 import InvitedDashboardCard from '@/components/invited-dashboard/InvitedDashboardCard';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
-
-export default function page() {
+export default function MydashboardPage() {
   return (
-    <div>
-      <QueryClientProvider client={queryClient}>
+    <div className='p-10'>
+      <div className='w-full max-w-screen-lg'>
+        {/* 나의 대시보드 리스트 */}
+        <MyDashboard />
+
+        {/* 초대받은 대시보드 */}
         <InvitedDashboardCard />
-      </QueryClientProvider>
+      </div>
     </div>
   );
 }
