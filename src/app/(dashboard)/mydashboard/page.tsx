6731badd78006f209efea863a449ab117ebb16ae
chanki.kim <chanki.kim89@gmail.com>
2025-02-08 12:33:32 +0900

    💄 style : 나의 데시보드 페이지 css 추가

--- src/app/(dashboard)/mydashboard/page.tsx
+++ src/app/(dashboard)/mydashboard/page.tsx
@@ -5,11 +5,10 @@ export default function MydashboardPage() {
   return (
     <div className='p-10'>
       <div className='w-full max-w-screen-lg'>
-        {/* 나의 대시보드 리스트 */}
-        <MyDashboard />
-
-        {/* 초대받은 대시보드 */}
-        <InvitedDashboardCard />
+        <div className='grid gap-10'>
+          <MyDashboard />
+          <InvitedDashboardCard />
+        </div>
       </div>
     </div>
   );
