4f88e6752d19f7c791857bd0c2e9913d58fe9b8d
chanki.kim <chanki.kim89@gmail.com>
2025-02-08 22:21:36 +0900

    ✨ feat : DateInput 컴포넌트 작업

---
+++ src/components/ui/Field/DateInput.tsx
@@ -0,0 +1,34 @@
+import { InputHTMLAttributes } from 'react';
+import Image from 'next/image';
+import DatePicker from 'react-datepicker';
+import { cn } from '@/utils/helper';
+import { BaseError, baseErrorClassName, baseFieldClassName, BaseItem, BaseLabel } from './Base';
+import calendarIcon from '@/assets/icons/calendar.svg';
+import { BaseField } from './types';
+import 'react-datepicker/dist/react-datepicker.css';
+
+type DateInputProps = BaseField &
+  Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'> & {
+    value: Date;
+    onChange: (value: Date | null) => void;
+  };
+
+export function DateInput({ label, error, value, onChange, onBlur, placeholder, required, className }: DateInputProps) {
+  return (
+    <BaseItem>
+      {label && <BaseLabel required={required}>{label}</BaseLabel>}
+      <div className='relative grid'>
+        <Image src={calendarIcon} alt='날짜선택' className={cn('pointer-events-none absolute left-4 top-1/2 z-20 h-auto w-4 -translate-y-1/2 opacity-50', value && 'opacity-100')} />
+        <DatePicker //
+          selected={value}
+          onChange={onChange}
+          onBlur={onBlur}
+          className={cn(baseFieldClassName, 'p-4 pl-10', error && baseErrorClassName, className)}
+          dateFormat='yyyy년 MM월 dd일'
+          placeholderText={placeholder}
+        />
+      </div>
+      {error && <BaseError>{error}</BaseError>}
+    </BaseItem>
+  );
+}
