797df2f3d848a90cde1987cbc512af27f9a89771
chanki.kim <chanki.kim89@gmail.com>
2025-02-08 22:32:20 +0900

    📝 chore : form sample page 수정

--- src/app/(dashboard)/form/page.tsx
+++ src/app/(dashboard)/form/page.tsx
@@ -116,7 +116,6 @@ export default function FormPage() {
                   placeholder='날짜를 입력해주세요'
                   required
                   {...field}
-                  readOnly
                 />
               );
             }}
