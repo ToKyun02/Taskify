a7290dea8a678446ebecac6c9a5a936c18af8fe7
Tokyun02 <developtk02@gmail.com>
2025-02-09 14:30:25 +0900

    :recycle: refactor : /dashboards axios 변경

--- src/apis/dashboards/index.ts
+++ src/apis/dashboards/index.ts
@@ -1,9 +1,9 @@
-import axiosHelper from '@/utils/network/axiosHelper';
+import axiosClientHelper from '@/utils/network/axiosClientHelper';
 import { BasePaginationParams, Dashboard, DashboardFormType, DashboardInvitation, DashboardInvitationResponse, DashboardsResponse, GetDashboardsParams, InviteDashboardType } from './types';
 
 // dashboard 목록 조회
 export const getDashboards = async ({ cursorId, page, size, navigationMethod }: GetDashboardsParams) => {
-  const response = await axiosHelper.get<DashboardsResponse>('/dashboards', {
+  const response = await axiosClientHelper.get<DashboardsResponse>('/dashboards', {
     params: {
       cursorId,
       page: page || 1,
@@ -16,31 +16,31 @@ export const getDashboards = async ({ cursorId, page, size, navigationMethod }: 
 
 // dashboard 생성
 export const createDashboard = async (data: DashboardFormType) => {
-  const response = await axiosHelper.post<Dashboard>('/dashboards', data);
+  const response = await axiosClientHelper.post<Dashboard>('/dashboards', data);
   return response.data;
 };
 
 // dashboard 상세 조회
 export const getDashboardDetails = async (id: number) => {
-  const response = await axiosHelper.get<Dashboard>(`/dashboards/${id}`);
+  const response = await axiosClientHelper.get<Dashboard>(`/dashboards/${id}`);
   return response.data;
 };
 
 // dashboard 수정
 export const updateDashboard = async (id: number, data: DashboardFormType) => {
-  const response = await axiosHelper.put<Dashboard>(`/dashboards/${id}`, data);
+  const response = await axiosClientHelper.put<Dashboard>(`/dashboards/${id}`, data);
   return response.data;
 };
 
 // dashboard 삭제
 export const deleteDashboard = async (id: number) => {
-  const response = await axiosHelper.delete<void>(`/dashboards/${id}`);
+  const response = await axiosClientHelper.delete<void>(`/dashboards/${id}`);
   return response.data;
 };
 
 // dashboard 초대 불러오기
 export const getDashboardInvitations = async (id: number, { page, size }: BasePaginationParams) => {
-  const response = await axiosHelper.get<DashboardInvitation>(`/dashboards/${id}/invitations`, {
+  const response = await axiosClientHelper.get<DashboardInvitation>(`/dashboards/${id}/invitations`, {
     params: {
       page: page || 1,
       size: size || 10,
@@ -51,12 +51,12 @@ export const getDashboardInvitations = async (id: number, { page, size }: BasePa
 
 // dashboard 초대
 export const inviteDashboard = async (id: number, data: InviteDashboardType) => {
-  const response = await axiosHelper.post<DashboardInvitationResponse>(`/dashboards/${id}/invitations`, data);
+  const response = await axiosClientHelper.post<DashboardInvitationResponse>(`/dashboards/${id}/invitations`, data);
   return response.data;
 };
 
 // dashboard 초대 취소
 export const cancelDashboardInvitation = async (id: number, invitationId: number) => {
-  const response = await axiosHelper.delete<void>(`/dashboards/${id}/invitations/${invitationId}`);
+  const response = await axiosClientHelper.delete<void>(`/dashboards/${id}/invitations/${invitationId}`);
   return response.data;
 };
