a7c42f7ce4b23c8baa06c90aae2eb0b3dfa5f0d9
chanki.kim <chanki.kim89@gmail.com>
2025-02-10 20:51:43 +0900

    ✨ feat : invitations api, queries, type 작업

---
+++ src/apis/invitations/index.ts
@@ -0,0 +1,29 @@
+import axiosClientHelper from '@/utils/network/axiosClientHelper';
+import { MyInvitationsParams, MyInvitationsResponse, myInvitationsResponseSchema, RespondToInvitationParams } from './types';
+import { DashboardInvitation, dashboardInvitationSchema } from '../dashboards/types';
+
+export const getMyInvitations = async ({ cursorId, size, title }: MyInvitationsParams) => {
+  const response = await axiosClientHelper.get<MyInvitationsResponse>('/invitations', {
+    params: {
+      size: size || 10,
+      ...(title && { title }), // 검색어가 있을경우에만(빈값 보내면 오류)
+      ...(cursorId && { cursorId }), //cursorId가 있을경우에만(빈값 보내면 오류)
+    },
+  });
+
+  const result = myInvitationsResponseSchema.safeParse(response.data);
+  if (!result.success) {
+    throw new Error('서버에서 받은 데이터가 예상과 다릅니다.');
+  }
+  return result.data;
+};
+
+export const RespondToInvitation = async (id: number, data: RespondToInvitationParams) => {
+  const response = await axiosClientHelper.put<DashboardInvitation>(`/invitations/${id}`, data);
+
+  const result = dashboardInvitationSchema.safeParse(response.data);
+  if (!result.success) {
+    throw new Error('서버에서 받은 데이터가 예상과 다릅니다.');
+  }
+  return result.data;
+};
