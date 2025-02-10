6ccf476f8351c4d3fb756cf76e21e0f9c8557bb1
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-02-10 20:10:44 +0900

    :bulb: comment: 사용방법 주석 추가

--- src/app/(dashboard)/dashboard/[id]/edit/page.tsx
+++ src/app/(dashboard)/dashboard/[id]/edit/page.tsx
@@ -1,5 +1,13 @@
 import GoBackLink from '@/components/ui/Link/GoBackLink';
 
+/*
+ * GoBackLink 컴포넌트
+ *
+ * - 이동할 링크 주소를 href prop으로 전달하면 됩니다.
+ * 예시:
+ *   <GoBackLink href="/mydashboard" />
+ */
+
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const id = (await params).id;
 
