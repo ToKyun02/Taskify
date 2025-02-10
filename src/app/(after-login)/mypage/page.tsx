b3dc21389b8a3a261cbf7163c83fbd97137b7585
Tokyun02 <developtk02@gmail.com>
2025-02-10 19:31:34 +0900

    :sparkles: feat : password edit 컴포넌트 구현 및 mypage 레이아웃 초기작업

---
+++ src/app/(after-login)/mypage/page.tsx
@@ -0,0 +1,9 @@
+import PasswordEdit from '@/components/profile/PasswordEdit';
+
+export default function MyPage() {
+  return (
+    <div className='mx-6 flex max-w-[670px] flex-col'>
+      <PasswordEdit />
+    </div>
+  );
+}
