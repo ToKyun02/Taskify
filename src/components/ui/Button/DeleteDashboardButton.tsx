b76a189067f4a2ee124040ff71e71c9db5a52895
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-02-10 15:50:42 +0900

    :sparkles: feat: 대시보드 삭제하기 버튼 구현

---
+++ src/components/ui/Button/DeleteDashboardButton.tsx
@@ -0,0 +1,3 @@
+export default function DeleteDashboardButton() {
+  return <button className='h-[52px] w-full rounded-lg border border-gray-30 bg-white text-lg font-medium hover:shadow-md md:h-[62px] md:w-[320px] md:text-2lg'>대시보드 삭제하기</button>;
+}
