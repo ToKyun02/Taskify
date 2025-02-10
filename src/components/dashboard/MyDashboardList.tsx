0429c59111370fdf318089c5b95c18ce86fd4f4a
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-02-10 15:50:17 +0900

    :sparkles: feat: 새로운 대시보드 추가 버튼 구현

--- src/components/dashboard/MyDashboardList.tsx
+++ src/components/dashboard/MyDashboardList.tsx
@@ -1,12 +1,11 @@
 'use client';
 
 import { useState } from 'react';
-import Image from 'next/image';
 import Link from 'next/link';
 import PaginationControls from '@/components/pagination/PaginationControls';
 import MyDashboardCard from '@/components/dashboard/MyDashboardCard';
-import plusIcon from '@/assets/icons/plus.svg';
 import { useDashboardsQuery } from '@/apis/dashboards/queries';
+import CreateDashboardButton from '../ui/Button/CreateDashboardButton';
 
 interface MyDashboardListProps {
   onAdd: () => void;
@@ -34,12 +33,7 @@ export default function MyDashboardList({ onAdd }: MyDashboardListProps) {
     <div className='grid gap-3'>
       <ul className='grid-row-6 md:grid-row-3 relative grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 lg:grid-rows-2'>
         <li>
-          <button type='button' className='flex h-[58px] w-full items-center justify-center gap-3 rounded-md border border-gray-30 bg-white p-5 hover:shadow-md md:h-[70px]' onClick={onAdd}>
-            새로운 대시보드
-            <span className='flex h-5 w-5 items-center justify-center rounded-sm bg-violet-10'>
-              <Image src={plusIcon} className='h-auto w-[10px]' alt='새로운 대시보드 만들기' />
-            </span>
-          </button>
+          <CreateDashboardButton onAdd={onAdd} />
         </li>
 
         {isLoading && [...Array(5)].map((item, index) => <SkeletionItem key={index} />)}
