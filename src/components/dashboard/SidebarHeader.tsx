2aa10c088f7d18dc23d824b681a68fa45f796910
chanki.kim <chanki.kim89@gmail.com>
2025-02-11 17:57:59 +0900

    💄 style : 사이드바 모바일 사용성 개선

--- src/components/dashboard/SidebarHeader.tsx
+++ src/components/dashboard/SidebarHeader.tsx
@@ -2,19 +2,20 @@
 
 import { useRef } from 'react';
 import Image from 'next/image';
+import { cn } from '@/utils/helper';
 import CreateDashboard from '@/components/dashboard/CreateDashboard';
 import { ModalHandle } from '@/components/ui/Modal/Modal';
 import add_box from '@/assets/icons/add_box.svg';
 
-export default function SidebarHeader() {
+export default function SidebarHeader({ open }: { open: boolean }) {
   const addDashboardModalRef = useRef<ModalHandle>(null);
 
   return (
     <>
-      <div className='mb-4 flex justify-center md:justify-between'>
-        <p className='hidden text-xs font-semibold text-gray-50 md:block'>Dash Boards</p>
+      <div className='my-3 flex h-6 items-center justify-between px-6 md:px-3 lg:px-4'>
+        <div className={cn('hidden whitespace-nowrap text-xs font-semibold text-gray-50 md:block', open && 'block')}>Dash Boards</div>
         <button type='button' onClick={() => addDashboardModalRef.current?.open()}>
-          <Image src={add_box} width={14} height={14} alt='add_button' className='cursor-pointer' />
+          <Image src={add_box} alt='add_button' className='pointer-events-none h-auto w-4' />
         </button>
       </div>
 
