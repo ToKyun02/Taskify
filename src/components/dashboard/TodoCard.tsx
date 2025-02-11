8d6615dc2ed470157822f81a92a114fc0d004cd5
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-02-11 15:21:59 +0900

    :lipstick: feat: 할일 카드 반응형 처리 작업

--- src/components/dashboard/TodoCard.tsx
+++ src/components/dashboard/TodoCard.tsx
@@ -9,17 +9,17 @@ export default function TodoCard() {
   const date = formatDate('2025-02-10');
 
   return (
-    <div className='flex w-full flex-col gap-1 rounded-md border border-gray-30 bg-white px-3 py-[5px]'>
-      <Image src={card_img} alt='할일 이미지' className='w-full' />
-      <div className='flex flex-col gap-1.5'>
-        <div className='flex flex-col gap-1.5'>
-          <span className='text-md font-medium text-gray-70'>새로운 일정 관리 Taskify</span>
+    <div className='flex w-full flex-col gap-1 rounded-md border border-gray-30 bg-white px-3 py-[5px] md:flex-row md:gap-2.5 lg:w-[314px] lg:flex-col'>
+      <Image src={card_img} alt='할일 이미지' className='w-full md:w-[90px] lg:w-full' />
+      <div className='flex flex-col gap-1.5 md:flex-1 md:flex-row lg:flex-col lg:gap-2'>
+        <div className='flex flex-col gap-1.5 md:flex-none lg:gap-2.5'>
+          <span className='text-md font-medium text-gray-70 lg:text-lg'>새로운 일정 관리 Taskify</span>
           <div className='flex gap-1.5'>
             <TagChip label='프로젝트' />
             <TagChip label='일반' />
           </div>
         </div>
-        <div className='flex justify-between'>
+        <div className='flex justify-between md:flex-auto md:items-end'>
           <div className='flex items-center gap-1'>
             <Image src={calendar} width={10} height={11} alt='캘린더' />
             <span className='text-xs font-medium text-gray-50'>{date}</span>
