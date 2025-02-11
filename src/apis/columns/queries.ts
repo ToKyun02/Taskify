0945ac4be93ee4a0cab93cd305cea4b0a9050110
Tokyun02 <developtk02@gmail.com>
2025-02-11 18:32:10 +0900

    :sparkles: feat : /columns query 작성

---
+++ src/apis/columns/queries.ts
@@ -0,0 +1,51 @@
+'use client';
+
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { deleteColumn, getColumns, postColumn, putColumn } from '.';
+import { ColumnForm } from './types';
+
+export const useColumnsQuery = (dashboardId: number) => {
+  return useQuery({
+    queryKey: ['columns', dashboardId],
+    queryFn: () =>
+      getColumns({
+        dashboardId,
+      }),
+  });
+};
+
+export const useColumnMutation = (dashboardId: number) => {
+  const queryClient = useQueryClient();
+
+  const post = useMutation({
+    mutationFn: (data: ColumnForm) => {
+      return postColumn(dashboardId, data);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['columns', dashboardId] });
+    },
+  });
+
+  const put = useMutation({
+    mutationFn: ({ id, formData }: { id: number; formData: ColumnForm }) => {
+      return putColumn(id, formData);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['columns', dashboardId] });
+    },
+  });
+
+  const remove = useMutation({
+    mutationFn: (id: number) => {
+      return deleteColumn(id);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['columns', dashboardId] });
+    },
+  });
+  return {
+    create: post.mutateAsync,
+    update: put.mutateAsync,
+    remove: remove.mutateAsync,
+  };
+};
