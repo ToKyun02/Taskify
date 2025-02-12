d161b4c98eb43cc70fad31b73fa43a2f0a56eb35
chanki.kim <chanki.kim89@gmail.com>
2025-02-12 19:23:36 +0900

    ✨ feat : 스택형 맴버리스트 개선 - 타입 수정 - 총인원 프롭스 추가

--- src/components/ui/Avatar/StackAvatars.tsx
+++ src/components/ui/Avatar/StackAvatars.tsx
@@ -1,26 +1,30 @@
+import { Member } from '@/apis/members/types';
 import Avatar from './Avatar';
 
 interface StackAvatarsProps {
-  members: { email: string; profileImageUrl?: string }[];
+  members: Pick<Member, 'email' | 'profileImageUrl'>[];
   visibleCount: number;
+  totalCount: number;
 }
 
-export default function StackAvatars({ members, visibleCount = 3 }: StackAvatarsProps) {
+export default function StackAvatars({ members, visibleCount = 3, totalCount }: StackAvatarsProps) {
   const chunkedMembers = members.slice(0, visibleCount);
-  const restMembersCount = members.length - visibleCount;
+  const restMembersCount = totalCount - visibleCount;
 
   return (
-    <ul className='flex pl-2 leading-none'>
-      {chunkedMembers.map((member) => (
-        <li key={member.email} className='-ml-2'>
-          <Avatar email={member.email} profileImageUrl={member.profileImageUrl} />
-        </li>
-      ))}
-      {restMembersCount > 0 && (
-        <li className='-ml-2'>
-          <span className='relative flex aspect-square h-full items-center justify-center rounded-full border-2 border-white bg-[#F4D7DA] font-medium text-[#D25B68]'>+{restMembersCount}</span>
-        </li>
-      )}
-    </ul>
+    <div className='pl-2'>
+      <ul className='flex leading-none'>
+        {chunkedMembers.map((member) => (
+          <li key={member.email} className='-ml-2 w-10'>
+            <Avatar email={member.email} profileImageUrl={member.profileImageUrl} />
+          </li>
+        ))}
+        {restMembersCount > 0 && (
+          <li className='-ml-2 w-10'>
+            <span className='relative flex aspect-square h-full items-center justify-center rounded-full border-2 border-white bg-[#F4D7DA] font-medium text-[#D25B68]'>+{restMembersCount}</span>
+          </li>
+        )}
+      </ul>
+    </div>
   );
 }
