4f3186cdccdc8ae6ff5b70e8e8ed5c2efcbde164
Tokyun02 <developtk02@gmail.com>
2025-02-12 17:07:58 +0900

    :recycle: refactor : 컬럼 중복 이름 생성 방지

--- src/components/columns/ColumnList.tsx
+++ src/components/columns/ColumnList.tsx
@@ -24,7 +24,7 @@ export default function ColumnList() {
           </li>
         ))
       )}
-      <AddColumnBtn dashboardId={dashbaordId} />
+      <AddColumnBtn dashboardId={dashbaordId} columns={data?.data} />
     </ul>
   );
 }
