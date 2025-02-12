830862fec75fdb1b02d8fbe6de70023852d00ffe
SeokChan-Lee <melee0826@gmail.com>
2025-02-12 19:51:35 +0900

    :fire: chore: 최소 글자수 제한 제거

--- src/constants/profileEdit.ts
+++ src/constants/profileEdit.ts
@@ -4,14 +4,12 @@ export const PROFILEEDIT_FORM_PLACEHOLDER = {
 
 export const PROFILEEDIT_FORM_VALID_LENGTH = {
   NICKNAME: {
-    MIN: 1,
     MAX: 10,
   },
 } as const;
 
 export const PROFILEEDIT_FORM_ERROR_MESSAGE = {
   NICKNAME: {
-    MIN: '닉네임을 작성해 주세요',
     MAX: '열 자 이하로 작성해 주세요',
   },
 } as const;
