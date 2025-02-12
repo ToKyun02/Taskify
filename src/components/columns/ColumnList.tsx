88878b1719e21d0b0c572d65606c2e030f3d04cb
Tokyun02 <developtk02@gmail.com>
2025-02-12 16:44:57 +0900

    :lipstick: feat : 컬럼 empty list 구현

--- src/components/columns/ColumnList.tsx
+++ src/components/columns/ColumnList.tsx
@@ -4,6 +4,7 @@ import { useColumnsQuery } from '@/apis/columns/queries';
 import { useParams } from 'next/navigation';
 import ColumnItem from './ColumnItem';
 import AddColumnBtn from './AddColumnBtn';
+import { isEmpty } from 'es-toolkit/compat';
 
 export default function ColumnList() {
   const params = useParams();
@@ -13,11 +14,16 @@ export default function ColumnList() {
   return (
     <ul className='flex flex-col lg:flex-row'>
       {isLoading && Array.from({ length: 3 }, (_, index) => <SkeletionItem key={index} />)}
-      {data?.data.map((column) => (
-        <li key={column.id} className='flex flex-col gap-4 border-b border-r-0 p-6 lg:min-h-[calc(100dvh-70px)] lg:border-b-0 lg:border-r'>
-          <ColumnItem column={column} />
-        </li>
-      ))}
+
+      {!isLoading && isEmpty(data?.data) ? (
+        <EmptyList />
+      ) : (
+        data?.data.map((column) => (
+          <li key={column.id} className='flex flex-col gap-4 border-b border-r-0 p-6 lg:min-h-[calc(100dvh-70px)] lg:border-b-0 lg:border-r'>
+            <ColumnItem column={column} />
+          </li>
+        ))
+      )}
       <AddColumnBtn dashboardId={dashbaordId} />
     </ul>
   );
@@ -34,6 +40,17 @@ export function SkeletionItem() {
         </div>
         <div className='ml-4 h-5 w-5 rounded-full bg-gray-300' />
       </div>
+      <div className='flex h-12 w-full items-center justify-center rounded bg-gray-200'>
+        <div className='h-6 w-6 rounded bg-gray-300' />
+      </div>
+    </li>
+  );
+}
+
+function EmptyList() {
+  return (
+    <li className='flex animate-pulse flex-col items-center gap-4 border-b border-r-0 p-6 lg:min-h-[calc(100dvh-70px)] lg:border-b-0 lg:border-r'>
+      <p className='gray-300 w-56 text-center'>컬럼이 비어있습니다.</p>
       <div className='flex h-12 w-full items-center justify-center rounded bg-gray-200'>
         <div className='h-6 w-6 rounded bg-gray-300' />
       </div>
