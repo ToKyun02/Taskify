a4972b05705f16d2661d78c3d53a2bbd7fcf9f10
rak517 <166088638+rak517@users.noreply.github.com>
2025-02-13 22:12:24 +0900

    Feat/106/todo card modal component (#127)
    * :sparkles: feat: 할일 카드 모달 생성
    
    * :sparkles: feat: 할 일 카드 상세정보 모달 구현
    
    * :bug: fix: 드롭다운, 이미지 수정
    
    * :fire: remove: 불필요한 console.log 삭제
    
    * :recycle: refactor: confirm 사용 부분 useConfirm으로 교체
    
    * Feat/40/mypage refactor (#126)
    
    * ♻️ refactor : 이미지 업로드 개선
    - 타입에 null 추가 (api에서 응답이 null이 올 수 있음)
    - 쿼리 개선 (zod validation에 용량 추가)
    - defaultValue 추가 (추가로 올린 이미지를 지우면, 원래 이미지가 보이게)
    
    * ♻️ refactor : 내정보 페이지 개선
    - 내정보를 ssr로 불러오도록 개선
    - 폼내부 공용 컴포넌트로 교체
    - 폼내부 로직 개선
    
    * 🐛 fix : 헤더 내정보에 프로필 이미지 노출 추가
    
    * 🐛 fix: useParams로 방문중인 대시보드 표시 개선 (#128)
    
    Co-authored-by: chanki.kim <chanki.kim89@gmail.com>
    
    * :lipstick: style: 로그인,회원가입 페이지 로고 png->svg변경 (#125)
    
    * :recycle: refactor: 코드 리뷰 반영
    
    * :bug: fix: Avartor 컴포넌트 profileImageUrl 오류 해결
    
    ---------
    
    Co-authored-by: 김찬기 (Chanki Kim) <44834561+cksrlcks@users.noreply.github.com>
    Co-authored-by: SeokChan-Lee <melee0826@gmail.com>
    Co-authored-by: chanki.kim <chanki.kim89@gmail.com>

---
+++ src/components/dashboard/DetailTodo.tsx
@@ -0,0 +1,160 @@
+'use client';
+
+import { forwardRef, useState } from 'react';
+import { Modal, ModalContent, ModalFooter, ModalHandle, ModalHeader } from '../ui/Modal/Modal';
+import { motion } from 'motion/react';
+import useAlert from '@/hooks/useAlert';
+import { useRouter } from 'next/navigation';
+import { Card } from '@/apis/cards/types';
+import { deleteCard } from '@/apis/cards';
+import CommentSection from './CommentSection';
+import useConfirm from '@/hooks/useConfirm';
+import Image from 'next/image';
+import Avatar from '../ui/Avatar/Avatar';
+import TagChip from '../ui/Chip/TagChip';
+import x_img from '@/assets/icons/x.svg';
+import kebob from '@/assets/icons/kebab.svg';
+import RoundChip from '../ui/Chip/RoundChip';
+import { getErrorMessage } from '@/utils/errorMessage';
+
+interface DetailTodoProps {
+  card: Card;
+}
+
+const NO_IMAGE_BASE_URL = 'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/taskify/task_image/';
+
+const DetailTodo = forwardRef<ModalHandle, DetailTodoProps>(({ card }, ref) => {
+  const alert = useAlert();
+  const confirm = useConfirm();
+  const router = useRouter();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleDeleteCard = async () => {
+    const userConfirmed = await confirm('이 카드를 삭제하시겠습니까?', {
+      buttons: {
+        ok: '삭제',
+        cancel: '취소',
+      },
+    });
+
+    if (!userConfirmed) return; // 취소 버튼을 누르면 진행 중단
+    try {
+      await deleteCard(card.id);
+      alert('카드가 삭제되었습니다.');
+      if (ref && 'current' in ref && ref.current) {
+        ref.current.close();
+      }
+      router.push('/dashboard');
+    } catch (err) {
+      const message = getErrorMessage(err);
+      alert(message);
+    }
+  };
+
+  const handleEditCard = () => {
+    alert('수정 모달 열기');
+  };
+
+  const handleXClick = () => {
+    if (ref && 'current' in ref) {
+      ref.current?.close();
+    }
+  };
+
+  return (
+    <Modal ref={ref}>
+      <ModalContent>
+        <ModalHeader className='relative flex justify-end'>
+          <div className='flex gap-4'>
+            <Image src={kebob} width={3} height={24} alt='메뉴' className='cursor-pointer' onClick={() => setIsDropdownOpen((prev) => !prev)} />
+            <Image src={x_img} width={24} height={24} alt='나가기' onClick={handleXClick} className='cursor-pointer' />
+          </div>
+          {isDropdownOpen && (
+            <motion.ul initial={{ scale: 0.8, y: -20 }} animate={{ scale: 1, y: 0 }} className='absolute right-0 top-[calc(100%+6px)] z-10 w-28 rounded-md border border-gray-30 bg-white p-[4px]'>
+              <li>
+                <button
+                  onClick={() => {
+                    setIsDropdownOpen(false);
+                    handleEditCard();
+                  }}
+                  className='flex h-10 w-full items-center justify-center rounded-md text-md hover:bg-violet-10 hover:font-medium hover:text-violet-20'
+                >
+                  수정하기
+                </button>
+              </li>
+              <li>
+                <button
+                  onClick={() => {
+                    setIsDropdownOpen(false);
+                    handleDeleteCard();
+                  }}
+                  className='flex h-10 w-full items-center justify-center rounded-md text-md hover:bg-violet-10 hover:font-medium hover:text-violet-20'
+                >
+                  삭제하기
+                </button>
+              </li>
+            </motion.ul>
+          )}
+        </ModalHeader>
+        <div className='flex flex-col gap-8 md:flex-row md:items-start'>
+          <div className='flex flex-1 flex-col gap-4'>
+            <div className='flex flex-col gap-4'>
+              <div className='flex flex-col gap-2 text-xl font-bold text-gray-70'>{card.title}</div>
+
+              <div className='md:hidden'>
+                <div className='flex h-[64px] w-[295px] justify-between rounded-lg border border-gray-30 px-4 py-1.5'>
+                  <div className='flex flex-col justify-between'>
+                    <span className='text-xs font-semibold text-black'>담당자</span>
+                    <div className='flex items-center gap-2'>
+                      <Avatar email={card.assignee.nickname} size='sm' />
+                      <span className='text-xs text-gray-70'>{card.assignee.nickname}</span>
+                    </div>
+                  </div>
+                  <div className='flex flex-col justify-between'>
+                    <span className='text-xs font-semibold text-black'>마감일</span>
+                    <span className='text-xs text-gray-70'>{card.dueDate}</span>
+                  </div>
+                </div>
+              </div>
+
+              <div className='flex items-center gap-3'>
+                <RoundChip label='To Do' />
+                <div className='h-5 w-[1px] bg-gray-30'></div>
+                {card.tags.map((tag) => (
+                  <TagChip key={tag} label={tag} />
+                ))}
+              </div>
+            </div>
+
+            <span className='text-md'>{card.description}</span>
+
+            {card.imageUrl && card.imageUrl !== NO_IMAGE_BASE_URL && <Image src={card.imageUrl} width={290} height={168} alt={card.title} className='object-cover' />}
+          </div>
+
+          <aside className='hidden flex-shrink-0 md:block'>
+            <div className='flex h-full w-[181px] flex-col gap-4 rounded-lg border border-gray-30 px-4 py-1.5'>
+              <div className='flex flex-col justify-between'>
+                <span className='text-xs font-semibold text-black'>담당자</span>
+                <div className='flex items-center gap-2'>
+                  <Avatar email={card.assignee.nickname} size='sm' profileImageUrl={card.assignee.profileImageUrl?.toString() ?? null} />
+                  <span className='text-xs text-gray-70'>{card.assignee.nickname}</span>
+                </div>
+              </div>
+              <div className='flex flex-col justify-between'>
+                <span className='text-xs font-semibold text-black'>마감일</span>
+                <span className='text-xs text-gray-70'>{card.dueDate}</span>
+              </div>
+            </div>
+          </aside>
+        </div>
+
+        <ModalFooter className='flex flex-col'>
+          <CommentSection cardId={card.id} columnId={card.columnId} dashboardId={card.dashboardId} />
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+});
+
+DetailTodo.displayName = 'DetailTodoModal';
+export default DetailTodo;
