5a30b0649a534cf1913cd8b9a0b83788babe09b7
김찬기 (Chanki Kim) <44834561+cksrlcks@users.noreply.github.com>
2025-02-14 02:42:15 +0900

    🐛 fix : middleware 개선 (#133)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,36 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
 
-const AFTER_LOGIN_DOMAIN = ['/mydashboard', '/dashboard/:path*', '/dashboard', '/mypage'] satisfies readonly string[];
+const AFTER_LOGIN_DOMAIN = ['/mydashboard', '/dashboard', '/mypage'] satisfies readonly string[];
 const BEFORE_LOGIN_DOMAIN = ['/faq', '/privacy', '/login', '/signup', '/'] satisfies readonly string[];
 
 export const middleware = async (request: NextRequest) => {
   const cookieStore = await cookies();
   const accessToken = cookieStore.get('accessToken');
+
+  const pathname = request.nextUrl.pathname;
+
   if (!accessToken?.value) {
-    if (AFTER_LOGIN_DOMAIN.includes(request.nextUrl.pathname)) return NextResponse.redirect(new URL('/login', request.url));
+    if (AFTER_LOGIN_DOMAIN.some((path) => pathname.startsWith(path))) {
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
   } else {
-    if (BEFORE_LOGIN_DOMAIN.includes(request.nextUrl.pathname)) return NextResponse.redirect(new URL('/mydashboard', request.url));
+    if (BEFORE_LOGIN_DOMAIN.includes(pathname)) {
+      return NextResponse.redirect(new URL('/mydashboard', request.url));
+    }
   }
 
   return NextResponse.next();
 };
 
 export const config = {
-  matcher: '/:path*',
+  matcher: [
+    {
+      source: '/:path*',
+      missing: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' },
+      ],
+    },
+  ],
 };
