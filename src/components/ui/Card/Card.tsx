910298d6bf0d09bd62f23924edc6a373e9ce58e0
chanki.kim <chanki.kim89@gmail.com>
2025-02-14 22:25:09 +0900

    💄 style : 스켈레톤 컴포넌트 작업

--- src/components/ui/Card/Card.tsx
+++ src/components/ui/Card/Card.tsx
@@ -7,3 +7,14 @@ export function Card({ children }: PropsWithChildren) {
 export function CardTitle({ children }: PropsWithChildren) {
   return <h3 className='mb-4 flex w-full items-center justify-between text-xl font-bold text-gray-70 md:text-2xl lg:mb-8'>{children}</h3>;
 }
+
+export function CardSkeleton({ count }: { count: number }) {
+  return [...Array(count)].map((_, index) => (
+    <Card key={index}>
+      <div className='mb-8 h-5 w-[30%] animate-pulse bg-gray-200' />
+      <div className='mb-4 h-5 w-full animate-pulse bg-gray-200' />
+      <div className='mb-14 h-5 w-full animate-pulse bg-gray-200' />
+      <div className='h-[42px] w-full animate-pulse bg-gray-200' />
+    </Card>
+  ));
+}
