68efdc362990bfb38b045567f4f397e684a60623
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-02-15 17:10:30 +0900

    :sparkles: feat: 댓글 영역 컴포넌트 쿼리 적용

--- src/components/dashboard/CommentSection.tsx
+++ src/components/dashboard/CommentSection.tsx
@@ -1,15 +1,15 @@
 'use client';
 
-import { deleteComment, getComments, postComment, putComment } from '@/apis/comments';
-import { Comment, CommentForm } from '@/apis/comments/types';
 import useAlert from '@/hooks/useAlert';
 import { formatDate } from '@/utils/formatDate';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Avatar from '../ui/Avatar/Avatar';
 import Button from '../ui/Button/Button';
 import { Textarea } from '../ui/Field';
-import { getErrorMessage } from '@/utils/errorMessage';
 import useConfirm from '@/hooks/useConfirm';
+import { useCommentsQuery, useDeleteComment, usePostComment, useUpdateComment } from '@/apis/comments/queries';
+import { CommentForm } from '@/apis/comments/types';
+import { getErrorMessage } from '@/utils/errorMessage';
 
 interface CommentSectionProps {
   cardId: number;
@@ -20,27 +20,13 @@ interface CommentSectionProps {
 export default function CommentSection({ cardId, columnId, dashboardId }: CommentSectionProps) {
   const alert = useAlert();
   const confirm = useConfirm();
-  const [comments, setComments] = useState<Comment[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
   const [content, setContent] = useState('');
 
-  useEffect(() => {
-    fetchComments();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cardId]);
+  const { data, isLoading } = useCommentsQuery({ cardId, size: 10 });
 
-  async function fetchComments() {
-    setIsLoading(true);
-    try {
-      const data = await getComments({ cardId, size: 10 });
-      setComments(data.comments);
-    } catch (err) {
-      const message = getErrorMessage(err);
-      alert(message);
-    } finally {
-      setIsLoading(false);
-    }
-  }
+  const postCommentMutation = usePostComment();
+  const updateCommentMutation = useUpdateComment(cardId);
+  const deleteCommentMutation = useDeleteComment(cardId);
 
   async function handleSubmitComment() {
     if (!content.trim()) return;
@@ -50,43 +36,36 @@ export default function CommentSection({ cardId, columnId, dashboardId }: Commen
       columnId,
       dashboardId,
     };
+
     try {
-      const newComment = await postComment(formData);
-      setComments((prev) => [newComment, ...prev]);
+      await postCommentMutation.mutateAsync(formData);
       setContent('');
     } catch (err) {
-      const message = getErrorMessage(err);
-      alert(message);
+      alert(getErrorMessage(err));
     }
   }
 
   async function handleEditComment(commentId: number, currentContent: string) {
     const newContent = prompt('댓글 수정하기', currentContent);
     if (!newContent) return;
+
     try {
-      const updated = await putComment(commentId, { content: newContent });
-      setComments((prev) => prev.map((c) => (c.id === commentId ? updated : c)));
+      await updateCommentMutation.mutateAsync({ id: commentId, putCommentForm: { content: newContent } });
     } catch (err) {
-      const message = getErrorMessage(err);
-      alert(message);
+      alert(getErrorMessage(err));
     }
   }
 
   async function handleDeleteComment(commentId: number) {
-    const userConfirmed = await confirm('이 카드를 삭제하시겠습니까?', {
-      buttons: {
-        ok: '삭제',
-        cancel: '취소',
-      },
+    const userConfirmed = await confirm('이 댓글을 삭제하시겠습니까?', {
+      buttons: { ok: '삭제', cancel: '취소' },
     });
-
     if (!userConfirmed) return;
+
     try {
-      await deleteComment(commentId);
-      setComments((prev) => prev.filter((c) => c.id !== commentId));
+      await deleteCommentMutation.mutateAsync(commentId);
     } catch (err) {
-      const message = getErrorMessage(err);
-      alert(message);
+      alert(getErrorMessage(err));
     }
   }
 
@@ -103,26 +82,28 @@ export default function CommentSection({ cardId, columnId, dashboardId }: Commen
       {isLoading && <div className='text-sm text-gray-50'>댓글 불러오는 중...</div>}
 
       {!isLoading &&
-        comments.map((comment) => (
-          <div key={comment.id} className='flex gap-3'>
-            <div>
-              <Avatar email={comment.author.nickname} size='sm' />
-            </div>
-            <div className='flex flex-col gap-2.5'>
-              <div className='flex flex-col'>
-                <div className='flex items-center gap-2'>
-                  <span className='text-md font-semibold text-gray-70'>{comment.author.nickname}</span>
-                  <span className='text-xs text-gray-40'>{formatDate(comment.createdAt)}</span>
+        data?.pages.map((page) =>
+          page.comments.map((comment) => (
+            <div key={comment.id} className='flex gap-3'>
+              <div>
+                <Avatar email={comment.author.nickname} size='sm' />
+              </div>
+              <div className='flex flex-col gap-2.5'>
+                <div className='flex flex-col'>
+                  <div className='flex items-center gap-2'>
+                    <span className='text-md font-semibold text-gray-70'>{comment.author.nickname}</span>
+                    <span className='text-xs text-gray-40'>{formatDate(comment.createdAt)}</span>
+                  </div>
+                  <span className='text-md text-gray-70'>{comment.content}</span>
                 </div>
-                <span className='text-md text-gray-70'>{comment.content}</span>
-              </div>
-              <div className='flex cursor-pointer gap-3 text-xs text-gray-40 underline'>
-                <span onClick={() => handleEditComment(comment.id, comment.content)}>수정</span>
-                <span onClick={() => handleDeleteComment(comment.id)}>삭제</span>
+                <div className='flex cursor-pointer gap-3 text-xs text-gray-40 underline'>
+                  <span onClick={() => handleEditComment(comment.id, comment.content)}>수정</span>
+                  <span onClick={() => handleDeleteComment(comment.id)}>삭제</span>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          )),
+        )}
     </div>
   );
 }
