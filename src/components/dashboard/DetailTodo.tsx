7fb9642639dfa82e639862b0873cd91b19fc0772
chanki.kim <chanki.kim89@gmail.com>
2025-02-17 15:28:50 +0900

    💄 style : 카드 상세 css 수정

--- src/components/dashboard/DetailTodo.tsx
+++ src/components/dashboard/DetailTodo.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { forwardRef, useState } from 'react';
+import { forwardRef, useEffect, useRef, useState } from 'react';
 import { Modal, ModalContent, ModalFooter, ModalHandle, ModalHeader } from '../ui/Modal/Modal';
 import { motion } from 'motion/react';
 import useAlert from '@/hooks/useAlert';
@@ -36,6 +36,8 @@ const DetailTodo = forwardRef<ModalHandle, DetailTodoProps>(({ card, onEdit }, r
   const columns = columnsData?.data ?? [];
   const foundColumn = columns.find((col) => col.id === card.columnId);
 
+  const menuRef = useRef<HTMLUListElement | null>(null);
+
   const handleDeleteCard = async () => {
     const userConfirmed = await confirm('이 카드를 삭제하시겠습니까?', {
       buttons: {
@@ -67,16 +69,39 @@ const DetailTodo = forwardRef<ModalHandle, DetailTodoProps>(({ card, onEdit }, r
     }
   };
 
+  useEffect(() => {
+    function handleClickOutside(e: MouseEvent) {
+      if (menuRef.current && !menuRef.current.contains(e.target as Node)) {
+        setIsDropdownOpen(false);
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
     <Modal ref={ref}>
       <ModalContent>
         <ModalHeader className='relative flex justify-end'>
-          <div className='flex gap-4'>
-            <Image src={kebob} width={3} height={24} alt='메뉴' className='cursor-pointer' onClick={() => setIsDropdownOpen((prev) => !prev)} />
-            <Image src={x_img} width={24} height={24} alt='나가기' onClick={handleXClick} className='cursor-pointer' />
+          <div className='flex'>
+            <button className='flex h-8 w-8 items-center justify-center rounded-sm hover:bg-gray-10' onClick={() => setIsDropdownOpen((prev) => !prev)}>
+              <Image src={kebob} width={3} height={24} alt='메뉴' className='cursor-pointer' />
+            </button>
+
+            <button className='flex h-8 w-8 items-center justify-center rounded-sm hover:bg-gray-10' onClick={handleXClick}>
+              <Image src={x_img} width={24} height={24} alt='나가기' className='cursor-pointer' />
+            </button>
           </div>
           {isDropdownOpen && (
-            <motion.ul initial={{ scale: 0.8, y: -20 }} animate={{ scale: 1, y: 0 }} className='absolute right-0 top-[calc(100%+6px)] z-10 w-28 rounded-md border border-gray-30 bg-white p-[4px]'>
+            <motion.ul
+              ref={menuRef}
+              initial={{ scale: 0.8, y: -20 }}
+              animate={{ scale: 1, y: 0 }}
+              className='absolute right-0 top-[calc(100%+6px)] z-10 w-28 rounded-md border border-gray-30 bg-white p-[4px] font-normal'
+            >
               <li>
                 <button
                   onClick={() => {
@@ -105,7 +130,7 @@ const DetailTodo = forwardRef<ModalHandle, DetailTodoProps>(({ card, onEdit }, r
         <div className='flex flex-col gap-8 md:flex-row md:items-start'>
           <div className='flex flex-1 flex-col gap-4'>
             <div className='flex flex-col gap-4'>
-              <div className='flex flex-col gap-2 text-xl font-bold text-gray-70'>{card.title}</div>
+              <div className='flex flex-col gap-2 break-all text-xl font-bold text-gray-70'>{card.title}</div>
 
               <div className='md:hidden'>
                 <div className='flex h-[64px] w-[295px] justify-between rounded-lg border border-gray-30 px-4 py-1.5'>
@@ -123,22 +148,24 @@ const DetailTodo = forwardRef<ModalHandle, DetailTodoProps>(({ card, onEdit }, r
                 </div>
               </div>
 
-              <div className='flex items-center gap-3'>
-                <RoundChip label={foundColumn?.title || ''} />
-                <div className='h-5 w-[1px] bg-gray-30'></div>
-                {card.tags.map((tag) => (
-                  <TagChip key={tag} label={tag} />
-                ))}
+              <div className='flex gap-3'>
+                <RoundChip label={foundColumn?.title || ''} className='' />
+                <div className='mt-1.5 h-5 w-[1px] bg-gray-30'></div>
+                <div className='flex flex-1 flex-wrap gap-1.5 pt-[2px]'>
+                  {card.tags.map((tag) => (
+                    <TagChip key={tag} label={tag} />
+                  ))}
+                </div>
               </div>
             </div>
 
-            <span className='text-md'>{card.description}</span>
+            <span className='break-all text-md'>{card.description}</span>
 
             {card.imageUrl !== DEFAULT_CARD_IMAGE_URL && <Image src={card.imageUrl} width={400} height={200} alt={card.title} className='object-cover' />}
           </div>
 
-          <aside className='hidden flex-shrink-0 md:block'>
-            <div className='flex h-full w-[181px] flex-col gap-4 rounded-lg border border-gray-30 px-4 py-1.5'>
+          <aside className='hidden w-[181px] flex-shrink-0 md:block'>
+            <div className='flex h-full w-full flex-col gap-4 rounded-lg border border-gray-30 px-4 py-1.5'>
               <div className='flex flex-col justify-between'>
                 <span className='text-xs font-semibold text-black'>담당자</span>
                 <div className='flex items-center gap-2'>
