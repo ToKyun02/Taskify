1aa7fb86c684e8d1c326ae7399fe33b6ad71d348
chanki.kim <chanki.kim89@gmail.com>
2025-02-23 15:20:15 +0900

    ♻️ refactor: 기존 auth 관련 api 삭제

--- src/app/api/auth/logout/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export const POST = async () => {
-  const response = new NextResponse(null, { status: 204 });
-
-  response.cookies.delete('accessToken');
-  return response;
-};
--- src/utils/network/axiosClientHelper.ts
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-const axiosClientHelper = axios.create({ baseURL: '/api' });
-
-export default axiosClientHelper;
